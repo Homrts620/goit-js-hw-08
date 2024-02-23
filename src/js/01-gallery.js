8866df9beb5b31f7d0367b27f288ae5304bf92d2
Homrts620 <hom620@gmail.com>
2024-02-23 17:31:48 +0100

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox.js';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery'); 
